f5204725581506577aa3669ca3c9dbea8fecefe8
Javier Fernandes <jf2876430@gmail.com>
2023-01-29 20:35:21 -0400

    cambiando el css

--- src/assets/components/WeatherCard.jsx
+++ src/assets/components/WeatherCard.jsx
@@ -10,31 +10,30 @@ const WeatherCard = ({ weather, temp }) => {
   return (
     <article className='card'>
       <header className='card__header'>
-        <h1 className='card__tittle'>Weather data</h1>
-        <h2 className='card__subtittle'>{weather && `${weather.name}, ${weather.sys.country}`}</h2>
+        <h1 className='card__tittle text--decoration'>{weather && `${weather.name}, ${weather.sys.country}`}</h1>
       </header>
       <section className='card__icon-container'>
         <img className='card__icon' src={weather && `http://openweathermap.org/img/wn/${weather.weather[0].icon}@4x.png`} alt="" />
+      </section>
+      <h3 className='card__temp text--decoration'>{isCelsisu ? `${temp?.celsius} °C` : `${temp?.farenhit} °F`}</h3>
 
-      </section>
-      <section className='card__info'>
-        <h3 className='card__description'>{weather?.weather[0].description}</h3>
-        <ul className='card__list'>
-          <li className='card__item'>
-            <span className='card__span'>Wind speed:</span> {weather?.wind.speed} m/s
-          </li>
-          <li className='card__item'>
-            <span className='card__span'>Clouds:</span> {weather?.clouds.all} %
-          </li>
-          <li className='card__item'>
-            <span className='card__span'>Pressure:</span> {weather?.main.pressure} hpa
-          </li>
-        </ul>
-      </section>
-      <h3 className='card__temp'>{isCelsisu ? `${temp?.celsius} °C` : `${temp?.farenhit} °F`}</h3>
+
       <footer className='card__footer'>
         <button className='card__btn' onClick={handleclick}>Change to {isCelsisu ? '°F' : '°C'}</button>
-
+        <section className='card__info'>
+          <h3 className='card__description'>{weather?.weather[0].description}</h3>
+          <ul className='card__list'>
+            <li className='card__item'>
+              <span className='card__span'>Wind speed:</span> {weather?.wind.speed} m/s
+            </li>
+            <li className='card__item'>
+              <span className='card__span'>Clouds:</span> {weather?.clouds.all} %
+            </li>
+            <li className='card__item'>
+              <span className='card__span'>Pressure:</span> {weather?.main.pressure} hpa
+            </li>
+          </ul>
+        </section>
       </footer>
     </article>
   )
